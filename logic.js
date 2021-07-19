63534053b1d6f28202e598da073821b64794ae7e
Makley Tibola Trichez <tibolamakley1@gmail.com>
2021-07-18 23:26:01 -0300

    Initializing project

---
+++ logic.js
@@ -0,0 +1,11 @@
+(() => {
+    getBorderValues();
+})();
+
+function getBorderValues() {
+    const borderTL = document.getElementById("border-top-left");
+    const borderTR = document.getElementById("border-top-right");
+    const borderBL = document.getElementById("border-bottom-left");
+    const borderBR = document.getElementById("border-bottom-right");
+
+}
